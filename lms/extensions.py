21c161dcd2dbf73b5ab882e1893a232b6a07b447
nkpythondeveloper <pyrpadeveloper@gmail.com>
2025-10-04 15:52:11 +0530

    extension added

---
+++ lms/extensions.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+mail = Mail()
