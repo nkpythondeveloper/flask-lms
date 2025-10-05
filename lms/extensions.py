4f95bb193b40bbf6f09ae31593b71608bbbdc993
nkpythondeveloper <pyrpadeveloper@gmail.com>
2025-10-05 16:26:13 +0530

    added tailwindcss, for working on templates

--- lms/extensions.py
+++ lms/extensions.py
@@ -3,9 +3,18 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from flask_wtf import CSRFProtect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 mail = Mail()
+csrf = CSRFProtect()
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["200 per day, 50 per hour"],
+    storage_uri="memory://",
+)
